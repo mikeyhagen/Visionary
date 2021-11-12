dc97ac74fd97d11698686a298ae6266f1f5265c9
mikeyhagen <mikeyhagen@gmail.com>
2021-11-11 22:47:23 -0600

    Added update and delete pages for Project, Stage, and stageupdate

--- project/urls.py
+++ project/urls.py
@@ -8,6 +8,13 @@ from project.views import (
 	StageCreateView,
 	StageupdateCreateView,
 	UserProjectList,
+	ProjectUpdateView,
+	ProjectDeleteView,
+	StageUpdateView,
+	StageDeleteView,
+	StageupdateUpdateView,
+	StageupdateDeleteView,
+
 
 
 
@@ -23,5 +30,10 @@ urlpatterns = [
 	path('<int:pk>/create/', StageCreateView.as_view(),name="stage_create"),
 	path('stageupdate/create/<int:pk>/', StageupdateCreateView.as_view(),name="stageupdate_create"),
 	path('user/<int:pk>/', UserProjectList.as_view(),name="user_project_list"),
-
+	path('<int:pk>/update', ProjectUpdateView.as_view(),name="project_update"),
+	path('<int:pk>/delete', ProjectDeleteView.as_view(),name="project_delete"),
+	path('stage/<int:pk>/update', StageUpdateView.as_view(),name="stage_update"),
+	path('stage/<int:pk>/delete', StageDeleteView.as_view(),name="stage_delete"),
+	path('stageupdate/<int:pk>/update', StageupdateUpdateView.as_view(),name="stageupdate_update"),
+	path('stageupdate/<int:pk>/delete', StageupdateDeleteView.as_view(),name="stageupdate_delete"),
 ]
--- project/views.py
+++ project/views.py
@@ -62,5 +62,36 @@ class UserProjectList(ListView):
     template_name = 'project/user_project_list.html'
     def get_queryset(self):
         queryset = Project.objects.filter(user = self.kwargs.get('pk'))
-        
+
         return queryset
+
+class ProjectUpdateView(UpdateView):
+    fields = ("name","user","description","target_completion","completion_date")
+    model = Project
+
+
+class ProjectDeleteView(DeleteView):
+    model = Project
+    success_url = reverse_lazy("project:todo")
+
+class StageUpdateView(UpdateView):
+    fields = ("type","target_completion","info","completion_date")
+    model = Stage
+
+
+class StageDeleteView(DeleteView):
+    model = Stage
+    def get_success_url(self):
+        projectId=self.object.project.pk
+        return reverse_lazy("project:project_detail",kwargs={'pk':projectId})
+
+class StageupdateUpdateView(UpdateView):
+    fields = ('text',)
+    model = Stageupdate
+
+
+class StageupdateDeleteView(DeleteView):
+    model = Stageupdate
+    def get_success_url(self):
+        projectId=self.object.stage.project.pk
+        return reverse_lazy("project:project_detail",kwargs={'pk':projectId})
